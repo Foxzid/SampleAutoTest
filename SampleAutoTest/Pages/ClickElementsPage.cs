0b88cd2a20183dfdb95ccb4e766328afd282c714
Foxzid <vhohlov12345@gmail.com>
2026-02-23 11:33:29 +0500

    Убрал ненужные методы, так как создал универсальный метод

--- SampleAutoTest/Pages/ClickElementsPage.cs
+++ SampleAutoTest/Pages/ClickElementsPage.cs
@@ -11,27 +11,6 @@ namespace SampleAutoTest.Pages
         private By ClickPage => By.Id("box");
 
         /// <summary>
-        /// Кнопка кота
-        /// </summary>
-        private By CatButton => By.XPath("//button[@onclick = 'catSound()']");
-        //*[text()='Cat']
-
-        /// <summary>
-        /// Кнопка собаки
-        /// </summary>
-        private By DogButton => By.XPath("//button[@onclick = 'dogSound()']");
-
-        /// <summary>
-        /// Кнопка свиньи
-        /// </summary>
-        private By PigButton => By.XPath("//button[@onclick = 'pigSound()']");
-
-        /// <summary>
-        /// Кнопка свиньи
-        /// </summary>
-        private By CowButton => By.XPath("//button[@onclick = 'cowSound()']");
-
-        /// <summary>
         /// Текст, который появится после нажатия на кнопку
         /// </summary>
         private By TextMessage => By.Id("demo");
@@ -42,30 +21,6 @@ namespace SampleAutoTest.Pages
             WaitElementVisible(elAnimal);
             Click(elAnimal);
             return this;
-        }
-        
-        public void ClickCatButton()
-        {
-            WaitElementVisible(CatButton);
-            Click(CatButton);
-        }
-
-        public void ClickDogButton()
-        {
-            WaitElementVisible(DogButton);
-            Click(DogButton);
-        }
-
-        public void ClickPigButton()
-        {
-            WaitElementVisible(PigButton);
-            Click(PigButton);
-        }
-
-        public void ClickCowButton()
-        {
-            WaitElementVisible(CowButton);
-            Click(CowButton);
         }
 
         public string GetTextButtonClick()
