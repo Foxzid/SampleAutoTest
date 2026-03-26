9302e35979cfd11221227879ad57a879d2953db8
Foxzid <vhohlov12345@gmail.com>
2026-03-26 14:02:38 +0500

    Добавил Allure для теста с таблицами

--- SampleAutoTest/Tests/TableTests.cs
+++ SampleAutoTest/Tests/TableTests.cs
@@ -1,6 +1,6 @@
-﻿using SampleAutoTest;
+﻿using Allure.Net.Commons;
+using Allure.NUnit.Attributes;
 using SampleAutoTest.Pages;
-using System.ComponentModel;
 
 namespace SampleAutoTest.Tests
 {
@@ -16,6 +16,10 @@ namespace SampleAutoTest.Tests
         [TestCase("Oranges", "$3.99")]
         [TestCase("Laptop", "$1200.00")]
         [TestCase("Marbles", "$1.25")]
+        [AllureName("Сравнение актуальных цен с ожидаемыми")]
+        [AllureDescription("Тест сравнивает актуальную цену на товар с ожидаемой")]
+        [AllureTag("Tables", "UI")]
+        [AllureSeverity(SeverityLevel.minor)]
         public void SpinnerPage_WaitSpinnerInvisible(string itemP, string expectedP)
         {
             var tablePage = Page();
